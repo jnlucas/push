d4298d368d9119d5e5149361d2a4527dd1b16e6d
logonrm <logonrm@fiap.com.br>
2018-03-19 23:00:05 -0300

    criado adapte e array list

---
+++ app/src/main/java/br/com/lucasfakes/fakezap/ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package br.com.lucasfakes.fakezap
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+
+/**
+ * Created by logonrm on 19/03/2018.
+ */
+class ViewPagerAdapter(fragmentManager: FragmentManager)
+    : FragmentPagerAdapter(fragmentManager){
+
+    val meusFragments = ArrayList<Fragment>()
+
+    override fun getItem(position: Int): Fragment {
+        return meusFragments[position]
+    }
+
+    override fun getCount(): Int {
+        return meusFragments.size
+    }
+
+    fun adicionar(fragment: Fragment){
+
+        meusFragments.add(fragment)
+        
+    }
+}
